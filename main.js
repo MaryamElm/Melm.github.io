d19b735217d0cb3be61474d1bfa6cc46ab2956d0
MaryamElm <melma086@uottawa.ca>
2022-06-09 18:56:16 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+
+$("#submit").click(function(){
+    alert("Etes vous sur des informations entrees?");
+  });
+  $(".notImplz").InnerHTML= "Not Implemented because of time constraint";
+});
